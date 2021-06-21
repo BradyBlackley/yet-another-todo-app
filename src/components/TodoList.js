3af6b6edcdcb664b5febdaee6cf53c042c376469
BradyBlackley <bradyblackley@mail.weber.edu>
2021-06-21 16:03:35 -0500

    added delete and read

---
+++ src/components/TodoList.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import TodoDelete from './TodoDelete'
+
+function TodoList() {
+
+    const View = {
+        TODOS: 0,
+        TODO_ADD_FORM: 1,
+        TODO_EDIT_FORM: 2,
+        TODO_DELETE_FORM: 3
+    }
+
+    const [todoItems, setTodoItems] = useState([]);
+    const [currentTodo, setCurrentTodo] = useState();
+    const [currentView, setCurrentView] = useState(View.TODOS);
+
+    const addClick = () => {
+        setCurrentTodo();
+        setCurrentView(View.TODO_ADD_FORM);
+    };
+
+    const saveTodo = todo => {
+        if(todo.text !== ""){
+            todo.id = todoItems.length + 1;
+            setTodoItems([...todoItems, todo])
+        }
+        setCurrentView("todos");
+    };
+
+    const deleteClick = evt => {
+        const todo = todoItems.find(t => t.id === parseInt(evt.target.value, 10));
+        setCurrentTodo(todo);
+        setCurrentView(View.TODO_DELETE_FORM);
+    }
+
+    const deleteTodo = (todoId) => {
+        const todo = todoItems.find(t => t.id === todoId);
+        console.log(todo);
+        todoItems.pop(todo);
+        setCurrentView(View.TODOS);
+    }
+
+    if(currentView === View.TODO_ADD_FORM) {
+        return (
+            <TodoForm saveTodo={saveTodo} currentTodo={currentTodo}/>
+        );
+    } else if (currentView === View.TODO_DELETE_FORM) {
+        return (
+            <TodoDelete saveTodo={saveTodo} currentTodo={currentTodo} deleteTodo={deleteTodo}/>
+        );
+    }
+    return (
+        <>
+        <div className="row">
+            <div className="col">
+                <h1>Todo App</h1>
+            </div>
+            <div className="col">
+                <button className="btn btn-primary" onClick={addClick}>Add</button>
+            </div>
+            <div className="row">
+                <ul className="list-group">
+                    {todoItems.map(i => <li key={i.id} className="list-group-item">{i.text}
+                    <button className="btn btn-secondary ms-2 me-2" value={i.id}>Edit
+                    </button><button className="btn btn-danger me-2" onClick={deleteClick}
+                     value={i.id}>Delete</button></li>)}
+                </ul>
+            </div>
+            
+        </div>
+        </>
+    );
+}
+
+export default TodoList;
---
+++ src/components/data.js
@@ -0,0 +1,4 @@
+export const emptyTodo = {
+    id: 0, 
+    text: ""
+};
